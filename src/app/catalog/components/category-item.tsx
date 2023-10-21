3236de21a62ffc3084abf89a14d6467b9ed31c30
Danilo Mendes <danilo.mirandha@gmail.com>
2023-10-21 15:31:21 -0300

    feat: Adicionando Pagina dos produtos por categoria

--- src/app/catalog/components/category-item.tsx
+++ src/app/catalog/components/category-item.tsx
@@ -1,5 +1,6 @@
 import { Category } from "@prisma/client";
 import Image from "next/image";
+import Link from "next/link";
 
 interface CategoryItemProps {
   category: Category;
@@ -7,7 +8,7 @@ interface CategoryItemProps {
 
 const CategoryItem = ({ category }: CategoryItemProps) => {
   return (
-
+    <Link href={`/category/${category.slug}`}>
       <div className="flex flex-col">
         <div className="bg-category-item-gradient flex h-[150px] w-full items-center justify-center rounded-tl-lg rounded-tr-lg">
           {/* IMAGEM */}
@@ -28,7 +29,7 @@ const CategoryItem = ({ category }: CategoryItemProps) => {
           <p className="text-center text-sm font-semibold">{category.name}</p>
         </div>
       </div>
-
+    </Link>
   );
 };
 
