eed50f70dc18be333fa9587e191b6384ac66b0bf
Danilo Mendes <danilo.mirandha@gmail.com>
2023-10-21 09:03:05 -0300

    feat: adicionando Categories e CategoriesItem

---
+++ src/app/(home)/components/categories.tsx
@@ -0,0 +1,15 @@
+import { prismaCliente } from "@/lib/prisma";
+import CategoryItem from "./category-item";
+
+const Categories = async () => {
+  const categories = await prismaCliente.category.findMany({});
+  return( 
+  <div className="grid grid-cols-2 gap-x-4 gap-y-2">
+    {categories.map((category) => (
+        <CategoryItem key={category.id} category={category} />
+    ))}
+  </div>
+)
+};
+
+export default Categories;
--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import Image from "next/image";
+import Categories from "./components/categories";
 
 
 
@@ -10,6 +11,10 @@ export default function Home() {
   return (
     <div className="p-5">
       <Image src="/banner-home-01.png" height={0} width={0} className="h-auto w-full" sizes="100vw" alt="até 55% de desconto esse mês!" />
+
+      <div className="mt-8">
+        <Categories/>
+      </div>
     </div>
   )
 }
