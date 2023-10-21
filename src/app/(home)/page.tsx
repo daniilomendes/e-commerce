ace9532ee4d5705eed96e945c982c319bb7f1382
Danilo Mendes <danilo.mirandha@gmail.com>
2023-10-21 11:11:52 -0300

    feat: Finalizando as ofertas e segundo banner

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -23,8 +23,11 @@ export default async function Home() {
       </div>
 
       <div className="mt-8">
+        <p className="font-bold uppercase pl-5 mb-3">Ofertas</p>
         <ProductList products={deals}/>
       </div>
+
+      <Image src="/banner-home-02.png" height={0} width={0} className="h-auto w-full px-5 mt-5" sizes="100vw" alt="até 55% de desconto em mouses!" />
     </div>
   )
 }
