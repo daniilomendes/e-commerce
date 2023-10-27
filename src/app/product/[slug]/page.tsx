64877c4a356ae39bb6fab941289c3e7eb7d09b3c
Danilo Mendes <danilo.mirandha@gmail.com>
2023-10-27 20:58:40 -0300

    feat: adicionando a página do Produto e seu components de images

---
+++ src/app/product/[slug]/page.tsx
@@ -0,0 +1,27 @@
+import { prismaCliente } from "@/lib/prisma";
+import ProductImages from "./components/product-images";
+
+interface ProductDetailsPageProps {
+    params: {
+        slug: string
+    }
+}
+
+const ProductDetailsPage = async ({params: {slug}}: ProductDetailsPageProps) => {
+
+    const product = await prismaCliente.product.findFirst({
+        where: {
+            slug: slug
+        }
+    })
+
+    if(!product) return null
+
+    return ( 
+        <div>
+            <ProductImages imageUrls={product.imageUrls} name={product.name}/>
+        </div>
+    );
+}
+ 
+export default ProductDetailsPage;
