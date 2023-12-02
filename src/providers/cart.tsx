0c5088755b2a388a060377353306699129feb4f2
Danilo Mendes <danilo.mirandha@gmail.com>
2023-12-02 13:17:41 -0300

    feat: adicionando CartItem component e melhorando a adição de produto no carrinho

--- src/providers/cart.tsx
+++ src/providers/cart.tsx
@@ -1,48 +1,68 @@
-"use client"
+"use client";
 
-import { Product } from "@prisma/client";
+import { ProductWithTotalPrice } from "@/helpers/product";
 import { ReactNode, createContext, useState } from "react";
 
-interface CartProduct extends Product{
-    quantity: number
+export interface CartProduct extends ProductWithTotalPrice {
+  quantity: number;
 }
 
 interface ICartContext {
-    products: CartProduct[]
-    cartTotalPrice: number
-    cartBasePrice: number
-    cartTotalDiscount: number
-    addProductToCart: (product: CartProduct) => void
+  products: CartProduct[];
+  cartTotalPrice: number;
+  cartBasePrice: number;
+  cartTotalDiscount: number;
+  addProductToCart: (product: CartProduct) => void;
 }
 
 export const CartContext = createContext<ICartContext>({
-    products: [],
-    cartTotalPrice: 0,
-    cartBasePrice: 0,
-    cartTotalDiscount: 0,
-    addProductToCart: () => {}
-})
+  products: [],
+  cartTotalPrice: 0,
+  cartBasePrice: 0,
+  cartTotalDiscount: 0,
+  addProductToCart: () => {},
+});
 
-const CartProvider = ({children}: {children: ReactNode}) => {
+const CartProvider = ({ children }: { children: ReactNode }) => {
+  const [products, setProducts] = useState<CartProduct[]>([]);
 
-    const [products, setProducts] = useState<CartProduct[]>([])
-
-    const addProductToCart = (product: CartProduct) => {
-        setProducts((prev) => [...prev, product])
+  const addProductToCart = (product: CartProduct) => {
+    const productIsAlreadyOnCart = products.some(
+      (cartProduct) => cartProduct.id === product.id,
+    );
+    if (productIsAlreadyOnCart) {
+      setProducts((prev) =>
+        prev.map((cartProduct) => {
+          if (cartProduct.id === product.id) {
+            return {
+              ...cartProduct,
+              quantity: cartProduct.quantity + product.quantity,
+            };
+          }
+          return cartProduct;
+        }),
+      );
+      return;
     }
 
-    return ( 
-        <CartContext.Provider 
-            value={{
-                products,
-                addProductToCart,
-                cartTotalPrice: 0,
-                cartBasePrice: 0,
-                cartTotalDiscount: 0
-        }}>
-            {children}
-        </CartContext.Provider>
-     );
-}
- 
+    
+
+    setProducts((prev) => [...prev, product]);
+  };
+
+  return (
+    <CartContext.Provider
+      value={{
+        products,
+        addProductToCart,
+        cartTotalPrice: 0,
+        cartBasePrice: 0,
+        cartTotalDiscount: 0,
+      }}
+    >
+      {children}
+    </CartContext.Provider>
+  );
+};
+
 export default CartProvider;
