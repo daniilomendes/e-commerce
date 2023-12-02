0036732606472c5e84e27ec68e3cdea54b5f246e
Danilo Mendes <danilo.mirandha@gmail.com>
2023-12-02 10:24:50 -0300

    feat: Adiconando componente DiscountBadge

--- src/app/product/[slug]/components/product-info.tsx
+++ src/app/product/[slug]/components/product-info.tsx
@@ -1,9 +1,9 @@
 "use client";
 
-import { Badge } from "@/components/ui/badge"
 import { Button } from "@/components/ui/button";
+import DiscountBadge from "@/components/ui/discount-badge";
 import { ProductWithTotalPrice } from "@/helpers/product"
-import { ArrowDownIcon, ArrowLeftIcon, ArrowRightIcon, TruckIcon } from "lucide-react"
+import { ArrowLeftIcon, ArrowRightIcon, TruckIcon } from "lucide-react"
 import { useState } from "react"
 
 
@@ -37,9 +37,9 @@ const ProductInfo = ({product: {name, basePrice, totalPrice, description, discou
             <div className="flex items-center gap-2">
                 <h1 className="text-xl font-bold">R$ {totalPrice.toFixed(2)}</h1>
                 {discountPercentage > 0 && (
-                    <Badge className="px-2 py-[2px]">
-                        <ArrowDownIcon size={14} /> {discountPercentage}%
-                    </Badge>
+                    <DiscountBadge>
+                       {discountPercentage}
+                    </DiscountBadge>
                 )}
             </div>
             
