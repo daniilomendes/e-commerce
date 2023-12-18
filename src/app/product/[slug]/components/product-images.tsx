5ac346643a484dd7d6d0c2e9eeba4477a3726c2c
Danilo Mendes <danilo.mirandha@gmail.com>
2023-12-18 16:20:55 -0300

    feat: garantindo que a página de detalhes do produto seja resposiva para o desktop

--- src/app/product/[slug]/components/product-images.tsx
+++ src/app/product/[slug]/components/product-images.tsx
@@ -11,13 +11,13 @@ interface ProductImagesProps {
 const ProductImages = ({ imageUrls, name }: ProductImagesProps) => {
   const [currentImage, setCurrentImage] = useState(imageUrls[0]);
 
-  const handleImageClick = (imageUrl:string) => {
-    setCurrentImage(imageUrl)
-  }
+  const handleImageClick = (imageUrl: string) => {
+    setCurrentImage(imageUrl);
+  };
 
   return (
-    <div className="flex flex-col">
-      <div className="flex h-[380px] w-full items-center justify-center bg-accent">
+    <div className="flex flex-col lg:min-h-full lg:w-3/5">
+      <div className="flex h-[380px] w-full items-center justify-center bg-accent lg:h-full lg:rounded-lg ">
         <Image
           src={currentImage}
           alt={name}
@@ -29,7 +29,7 @@ const ProductImages = ({ imageUrls, name }: ProductImagesProps) => {
         />
       </div>
 
-      <div className="mt-8 grid grid-cols-4 gap-4 px-5">
+      <div className="mt-8 grid grid-cols-4 gap-4 px-5 lg:px-0">
         {imageUrls.map((imageUrl) => (
           <button
             key={imageUrl}
--- src/app/product/[slug]/components/product-info.tsx
+++ src/app/product/[slug]/components/product-info.tsx
@@ -4,84 +4,99 @@ import { Button } from "@/components/ui/button";
 import DiscountBadge from "@/components/ui/discount-badge";
 import { ProductWithTotalPrice } from "@/helpers/product";
 import { CartContext } from "@/providers/cart";
-import { ArrowLeftIcon, ArrowRightIcon, TruckIcon } from "lucide-react"
-import { useContext, useState } from "react"
-
+import { ArrowLeftIcon, ArrowRightIcon, TruckIcon } from "lucide-react";
+import { useContext, useState } from "react";
 
 interface ProductInfoProps {
-    product: ProductWithTotalPrice
+  product: ProductWithTotalPrice;
 }
 
-const ProductInfo = ({product}: ProductInfoProps) => {
+const ProductInfo = ({ product }: ProductInfoProps) => {
+  const [quantity, setQuantity] = useState(1);
 
-    const [quantity, setQuantity] = useState(1)
+  const { addProductToCart } = useContext(CartContext);
 
-    const {addProductToCart} = useContext(CartContext)
+  const handleDecreaseQuantityClick = () => {
+    setQuantity((prev) => (prev === 1 ? prev : prev - 1));
+  };
 
-    const handleDecreaseQuantityClick = () => {
-        setQuantity((prev) => (prev === 1 ? prev : prev - 1))
-    }
+  const handleIncreaseQuantityClick = () => {
+    setQuantity((prev) => prev + 1);
+  };
 
-    const handleIncreaseQuantityClick = () => {
-        setQuantity((prev) => prev + 1)
-    }
+  const handleAddToCartClick = () => {
+    addProductToCart({ ...product, quantity });
+  };
 
-    const handleAddToCartClick = () => {
-        addProductToCart({...product, quantity})
-    }
+  return (
+    <div className="flex flex-col px-5 lg:w-[40%] lg:rounded-lg lg:bg-accent lg:p-10">
+      <h2 className="text-lg lg:text-2xl">{product.name}</h2>
 
-    return (
-        <div className="flex flex-col px-5">
-            <h2 className="text-lg">{product.name}</h2>
+      <div className="flex items-center gap-2">
+        <h1 className="text-xl font-bold lg:text-3xl">
+          R$ {product.totalPrice.toFixed(2)}
+        </h1>
+        {product.discountPercentage > 0 && (
+          <DiscountBadge className="lg:text-base">{product.discountPercentage}</DiscountBadge>
+        )}
+      </div>
 
-            <div className="flex items-center gap-2">
-                <h1 className="text-xl font-bold">R$ {product.totalPrice.toFixed(2)}</h1>
-                {product.discountPercentage > 0 && (
-                    <DiscountBadge>
-                       {product.discountPercentage}
-                    </DiscountBadge>
-                )}
-            </div>
-            
-            {product.discountPercentage > 0 && (
-                <p className="text-sm opacity-75 line-through">R$ {Number(product.basePrice).toFixed(2)}</p>
-            )}
+      {product.discountPercentage > 0 && (
+        <p className="text-sm line-through opacity-75 lg:text-base">
+          R$ {Number(product.basePrice).toFixed(2)}
+        </p>
+      )}
 
-            <div className="flex items-center gap-2 mt-4">
-                <Button size="icon" variant="outline" onClick={handleDecreaseQuantityClick}>
-                    <ArrowLeftIcon size={16}/>
-                </Button>
+      <div className="mt-4 flex items-center gap-2">
+        <Button
+          size="icon"
+          variant="outline"
+          onClick={handleDecreaseQuantityClick}
+        >
+          <ArrowLeftIcon size={16} />
+        </Button>
 
-                <span>{quantity}</span>
-                
-                <Button size="icon" variant="outline" onClick={handleIncreaseQuantityClick}>
-                    <ArrowRightIcon size={16}/>
-                </Button>
-            </div>
+        <span>{quantity}</span>
 
-            <div className="flex flex-col gap-3 mt-8">
-                <h3 className="font-bold">Descrição</h3>
-                <p className="opacity-60 text-sm text-justify">{product.description}</p>
-            </div>
+        <Button
+          size="icon"
+          variant="outline"
+          onClick={handleIncreaseQuantityClick}
+        >
+          <ArrowRightIcon size={16} />
+        </Button>
+      </div>
 
-            <Button className="mt-8 uppercase font-bold" onClick={handleAddToCartClick}>
-                Adicionar ao carrinho
-            </Button>
+      <div className="mt-8 flex flex-col gap-3">
+        <h3 className="font-bold">Descrição</h3>
+        <p className="text-justify text-sm opacity-60">{product.description}</p>
+      </div>
 
-            <div className="bg-accent flex items-center px-5 py-2 justify-between mt-5 rounded-lg">
-                <div className="flex items-center gap-2">
-                    <TruckIcon />
+      <Button
+        className="mt-8 font-bold uppercase"
+        onClick={handleAddToCartClick}
+      >
+        Adicionar ao carrinho
+      </Button>
 
-                    <div className="flex flex-col">
-                        <p className="text-xs"> Entrega via <span className="font-bold">ECPacket</span></p>
-                        <p className="text-[#8162FF] text-xs">Envio para <span className="font-bold">todo Brasil</span></p>
-                    </div>
-                </div>
+      <div className="mt-5 flex items-center justify-between rounded-lg bg-accent px-5 py-2 lg:bg-[#2A2A2A]">
+        <div className="flex items-center gap-2">
+          <TruckIcon />
 
-                <p className="text-xs font-bold">Frete grátis</p>
-            </div>
+          <div className="flex flex-col">
+            <p className="text-xs">
+              Entrega via <span className="font-bold">ECPacket</span>
+            </p>
+            <p className="text-xs text-[#8162FF]">
+              Envio para <span className="font-bold">todo Brasil</span>
+            </p>
+          </div>
         </div>
-    )
-}
 
-export default ProductInfo
+        <p className="text-xs font-bold">Frete grátis</p>
+      </div>
+    </div>
+  );
+};
+
+export default ProductInfo;
