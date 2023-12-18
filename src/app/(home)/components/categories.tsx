876cb5cfa5dfe812832c8ebbc1035dc369bb1ec5
Danilo Mendes <danilo.mirandha@gmail.com>
2023-12-18 05:57:49 -0300

    feat: garantindo a responsividade das categorias para desktop

--- src/app/(home)/components/categories.tsx
+++ src/app/(home)/components/categories.tsx
@@ -3,9 +3,9 @@ import CategoryItem from "./category-item";
 
 const Categories = async () => {
   const categories = await prismaCliente.category.findMany({});
-  
+
   return (
-    <div className="grid grid-cols-2 gap-x-4 gap-y-2">
+    <div className="grid grid-cols-2 gap-x-4 gap-y-2 lg:grid-cols-6">
       {categories.map((category) => (
         <CategoryItem key={category.id} category={category} />
       ))}
