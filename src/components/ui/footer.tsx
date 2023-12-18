0296d33658a700aca713040f299116d64ed28221
Danilo Mendes <danilo.mirandha@gmail.com>
2023-12-18 14:13:13 -0300

    feat: assegurando que o Footer esta resposivo para desktop

--- src/components/ui/footer.tsx
+++ src/components/ui/footer.tsx
@@ -1,9 +1,11 @@
 const Footer = () => {
-    return ( 
-        <div className="bg-accent px-8 py-4 text-[0.625rem] opacity-75">
-            © 2023 Copyright <span className="font-semibold">ECD Store</span>
-        </div>
-     );
-}
- 
+  return (
+    <div className="bg-accent px-8 py-4 text-[0.625rem] opacity-75 lg:text-sm">
+      <div className="lg:container mx-auto">
+        © 2023 Copyright <span className="font-semibold">ECD Store</span>
+      </div>
+    </div>
+  );
+};
+
 export default Footer;
