e27dc9b418ec16e846c36a50144994fc4e1e1e5c
Danilo Mendes <danilo.mirandha@gmail.com>
2023-12-18 11:23:03 -0300

    fix: resolvendo erro na tipagem do typescript

--- src/app/product/[slug]/page.tsx
+++ src/app/product/[slug]/page.tsx
@@ -6,45 +6,51 @@ import ProductList from "@/components/ui/product-list";
 import SectionTitle from "@/components/ui/section-title";
 
 interface ProductDetailsPageProps {
-    params: {
-        slug: string
-    }
+  params: {
+    slug: string;
+  };
 }
 
-const ProductDetailsPage = async ({params: {slug}}: ProductDetailsPageProps) => {
-
-    const product = await prismaCliente.product.findFirst({
-        where: {
-            slug: slug
-        },
+const ProductDetailsPage = async ({
+  params: { slug },
+}: ProductDetailsPageProps) => {
+  const product = await prismaCliente.product.findFirst({
+    where: {
+      slug: slug,
+    },
+    include: {
+      category: {
         include: {
-            category: {
-                include: {
-                    products: {
-                        where: {
-                            slug: {
-                                not: slug
-                            }
-                        }
-                    }
-                }
-            }
-        }
-    })
+          products: {
+            where: {
+              slug: {
+                not: slug,
+              },
+            },
+          },
+        },
+      },
+    },
+  });
 
-    if(!product) return null
+  if (!product) return null;
 
-    return ( 
-        <div className="flex flex-col gap-8 pb-8">
-            <ProductImages imageUrls={product.imageUrls} name={product.name}/>
-            <ProductInfo product={computeProductTotalPrice(product)}/>
-            
-            <div>
-                <SectionTitle>Produtos Recomendados</SectionTitle>
-                <ProductList products={product.category.products}/>
-            </div>
-        </div>
-    );
-}
- 
+  return (
+    <div className="flex flex-col gap-8 pb-8">
+      <ProductImages imageUrls={product.imageUrls} name={product.name} />
+      <ProductInfo
+        product={{
+          ...product,
+          totalPrice: computeProductTotalPrice(product),
+        }}
+      />
+
+      <div>
+        <SectionTitle>Produtos Recomendados</SectionTitle>
+        <ProductList products={product.category.products} />
+      </div>
+    </div>
+  );
+};
+
 export default ProductDetailsPage;
