511c4680a5696945570822222138733350b18aa5
Danilo Mendes <danilo.mirandha@gmail.com>
2023-12-19 15:49:39 -0300

    feat: garantindo que a tabela de pedidos está funcionando na página de pedidos

--- src/components/ui/order-item.tsx
+++ src/components/ui/order-item.tsx
@@ -11,7 +11,7 @@ import OrderProductItem from "./order-product-item";
 import { Separator } from "@/components/ui/separator";
 import { useMemo } from "react";
 import { computeProductTotalPrice } from "@/helpers/product";
-import { getOrderStatus } from "../helpers/status";
+import { getOrderStatus } from "../../app/(shop)/orders/helpers/status";
 
 interface OrderItemProps {
   order: Prisma.OrderGetPayload<{
