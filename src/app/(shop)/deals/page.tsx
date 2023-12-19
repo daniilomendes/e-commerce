8f79d6e4642f54ac047c58f4d9243caf1affb25f
Danilo Mendes <danilo.mirandha@gmail.com>
2023-12-19 15:56:23 -0300

    feat: garantindo a responsividade na página de ofertas

--- src/app/(shop)/deals/page.tsx
+++ src/app/(shop)/deals/page.tsx
@@ -14,13 +14,13 @@ const DealsPage = async () => {
   });
 
   return (
-    <div className="flex flex-col gap-8 p-5">
+    <div className="flex flex-col gap-8 p-5 lg:container lg:gap-10 lg:py-10">
       <Badge variant="heading">
         <PercentIcon size={16} />
         Ofertas
       </Badge>
 
-      <div className="grid grid-cols-2 gap-8">
+      <div className="grid grid-cols-2 gap-8 lg:grid-cols-6">
         {deals.map((product) => (
           <ProductItem
             key={product.id}
