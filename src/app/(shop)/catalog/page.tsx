332cc5b4a77ac0e864063c01067c796752671a06
Danilo Mendes <danilo.mirandha@gmail.com>
2023-12-19 15:58:43 -0300

    feat: garantindo a responsividade na página de Catalogo

--- src/app/(shop)/catalog/page.tsx
+++ src/app/(shop)/catalog/page.tsx
@@ -7,13 +7,13 @@ const CatalogPage = async () => {
   const categories = await prismaCliente.category.findMany({});
 
   return (
-    <div className="flex flex-col gap-8 p-5">
+    <div className="flex flex-col gap-8 p-5 lg:container lg:mx-auto lg:py-10">
       <Badge variant="heading">
         <ShapesIcon size={16} />
         Catálogo
       </Badge>
 
-      <div className="grid grid-cols-2 gap-8">
+      <div className="grid grid-cols-2 gap-8 lg:grid-cols-3">
         {categories.map((category) => (
           <CategoryItem key={category.id} category={category} />
         ))}
