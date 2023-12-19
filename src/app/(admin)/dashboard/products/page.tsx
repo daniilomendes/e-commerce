bebb4739966a1c8ca1614fc5851fda55cacaee21
Danilo Mendes <danilo.mirandha@gmail.com>
2023-12-19 12:08:52 -0300

    feat: garantindo que o botão de adicionar e a quantidade de produtos estão sendo mostrados na pagina de Produtos

--- src/app/(admin)/dashboard/products/page.tsx
+++ src/app/(admin)/dashboard/products/page.tsx
@@ -1,10 +1,30 @@
+import { Badge } from "@/components/ui/badge";
+import { Button } from "@/components/ui/button";
 import { prismaCliente } from "@/lib/prisma";
+import { PackageIcon, PlusIcon } from "lucide-react";
 
 const ProductsPage = async () => {
+  const products = await prismaCliente.product.findMany();
 
-    const products = await prismaCliente.product.findMany()
+  return (
+    <div className="flex w-full flex-col gap-10 p-10">
+      <Badge variant="heading">
+        <PackageIcon size={16} />
+        Produtos
+      </Badge>
 
-    return ( <h1>Products</h1> );
-}
- 
+      <div className="flex w-full items-center justify-between">
+        <p className="text-lg font-bold">
+          Produtos encontrados: {products.length}
+        </p>
+
+        <Button className="flex gap-2">
+          <PlusIcon size={18}/>
+          Adicionar produto
+        </Button>
+      </div>
+    </div>
+  );
+};
+
 export default ProductsPage;
